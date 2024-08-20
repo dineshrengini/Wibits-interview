e2db974212aee83096e2e83f54a78e6dec6d120f
dineshrengini <dinesharumugam58@gmail.com>
2024-08-20 09:27:57 +0530

    Rent cards are dynamicly generaqted using ngFor property and popins font added into the cards.

--- src/app/Componednts/HomepageComponent/rent-cards/rent-cards.component.ts
+++ src/app/Componednts/HomepageComponent/rent-cards/rent-cards.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rent-cards',
   templateUrl: './rent-cards.component.html',
-  styleUrl: './rent-cards.component.css'
+  styleUrls: ['./rent-cards.component.css']
 })
 export class RentCardsComponent {
-
+  properties = [
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-1.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-2.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-3.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-4.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-5.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-6.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-7.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+    {
+      imageUrl: '../../../../assets/Homepage-images/rent-card-8.jpeg',
+      title: 'Al Mir Building – Showroom For Rent',
+      price: 'OMR 1,500 /per month'
+    },
+  ];
 }
